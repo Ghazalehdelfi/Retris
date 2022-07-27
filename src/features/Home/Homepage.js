bccf8854bb8de68a05081a63c5c4025a175ea5f6
ghazaleh delfi <ghazalehdelfi@gmail.com>
2022-07-26 22:19:36 -0400

    update(home): change layout
    add team states on home dashboard, only add a profile icon and new retro
    button instead of the previous tabs layout

--- src/features/Home/Homepage.js
+++ src/features/Home/Homepage.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import PropTypes from 'prop-types';
+import IconButton from '@mui/material/IconButton';
+import Fab from '@mui/material/Fab';
+import Typography from '@mui/material/Typography';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
@@ -10,73 +15,33 @@ import NewRetro from '../RetroBoard/NewRetro'
 import PrevRetro from '../RetroBoard/PrevRetro'
 import Token from '../Token'
 import './Homepage.css'
+import { padding } from '@mui/system';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            {children}
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-  };
-
-function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
-const HomeTabs = () => {
+const Home = () => {
     
-    const [value, setValue] = React.useState(0);
     const navigate = useNavigate();
-
-    const handleChange = (event, newValue) => {
-    setValue(newValue);
-    };
 
     return(
         <div>
-            <div className='Actionbar'>
-                <Button sx={{width: '120px', marginLeft:'auto', marginTop:'20px', marginRight:'20px', padding:'10px', float:'right'}} 
-                  onClick={()=>{navigate("/home/newretro")}} variant="contained">
-                    Join retro
-                </Button>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs sx={{borderBottom:'1px grey'}} value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label="Dashboard" {...a11yProps(0)} />
-                        <Tab label="Give tokens" {...a11yProps(1)} />
-                        <Tab label="Previous retro" {...a11yProps(2)} />
-                    </Tabs>
-                </Box>
-                <TabPanel value={value} index={0}>
-                    {<Dashboard />}
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                  {<Token/>}
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                  {<PrevRetro/>}
-                </TabPanel>
-            </div>
+          <div className='header'>
+            <Typography variant="h5">
+              Team Name Dashboard
+            </Typography>
+            <IconButton sx={{color:'white', marginLeft: 'auto'}} aria-label="upload picture" component="span">
+              <PersonIcon />
+            </IconButton>
+            <Button variant="text" sx={{color:'white'}}>Log out</Button>
+          </div>
+          <div className='container'>
+            <Dashboard />
+            <Fab 
+              sx={{position: 'absolute', right:'20px', bottom:'20px', backgroundColor:'#201E22'}} 
+              color="primary" 
+              aria-label="add"
+              onClick= {() => navigate("/home/newretro")}>
+              <AddIcon />
+            </Fab>
+          </div>
         </div>
     )
 }
@@ -84,7 +49,7 @@ const HomeTabs = () => {
 const HomePage = () => {
   return(
     <Routes> 
-      <Route exact path="/" element={<HomeTabs/>}/>
+      <Route exact path="/" element={<Home/>}/>
       <Route path="/newretro" element={<NewRetro/>}/>
     </Routes>
   )
