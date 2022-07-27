cce35f9ff4047aed24d63fd17f1fb5456f6ea705
ghazaleh delfi <ghazalehdelfi@gmail.com>
2022-07-26 22:22:24 -0400

    update(dashboard): add in placeholder for stats

--- src/features/Home/Dashboard/Dashboard.js
+++ src/features/Home/Dashboard/Dashboard.js
@@ -1,8 +1,20 @@
+import React from "react";
+import {Paper, Grid} from "@mui/material";
+
 const Dashboard = () => {
-    return(
-        <div>
-            dashboard
-        </div>)
+
+  return (
+    <div>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper sx={{height:'calc(50vh - 100px)'}}>Personal Stats</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper sx={{height:'calc(50vh - 100px)'}}>Team Stats</Paper>
+          </Grid>
+        </Grid>
+    </div>
+)
 }
 
 export default Dashboard
